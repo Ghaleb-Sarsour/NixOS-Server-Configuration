5ddb1157520a928300a26024a5a9257d574f889f
Ghaleb Sarsour <ghaleb.sarsour2007@gmail.com>
2025-06-28 14:10:58 -0400

    Created base flake.nix and added configuration.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Main flake for server";
+
+  inputs = {
+    # Package Sources.
+      nixpkgs.url = "nixpkgs/nixos-24.11"; # Change this line to update the package sources.
+  };
+
+  outputs =  {nixpkgs, ...}:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux"; # Change this if you are using a different system architecture.
+      pkgs = nixpkgs.legacyPackages.${system}; # Packages for the system.
+    
+    in
+    {
+      # System Configuration.
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+        }; 
+      };
+
+      # NixOS Modules
+      modules = [
+        ./configuration.nix
+      ];
+
+    };
+}
